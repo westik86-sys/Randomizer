be31725d351128c355f83e6fdcf6cb38cc83f74e
Westik86 <westik86@gmail.com>
2026-03-01 13:48:53 +0300

    Initial Commit

---
+++ Randomizer/RandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomizerApp.swift
+//  Randomizer
+//
+//  Created by Павел Коростелев on 01.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
